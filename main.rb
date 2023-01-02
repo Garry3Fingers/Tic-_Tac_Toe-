5724c948ea14dc3b2fda439ca1c2d5a00e9711f9
Garry3Fingers <www.vlad78@yandex.ru>
2023-01-02 21:19:22 +0300

    Implement a create_player method
    It need to refactor

--- main.rb
+++ main.rb
@@ -10,17 +10,62 @@ class Board
   end
 end
 
-class Players
+class Player
+  attr_accessor :name, :sign
 
+  def initialize(name, sign)
+    @name = name
+    @sign = sign
+  end
+
+  def to_s
+    "name: #{name} sign: #{sign}"
+  end
 end
 
-class PlayerOne < Players
+class InvalidInput < StandardError; end
 
+module Game
+  def self.input
+    gets.chomp
+  end
+
+  def self.create_player
+    if defined?(FIRST_PLAYER) == 'constant'
+      puts 'Enter the second player name'
+    else
+      puts 'Enter the first player name'
+    end
+
+    name = Game.input
+
+    if defined?(FIRST_PLAYER) == 'constant'
+      puts "Enter a letter for the second player\'s sign\n"\
+      "It mustn't be #{FIRST_PLAYER.sign}"
+    else
+      puts 'Enter a letter for the first player\'s sign'
+    end
+
+    loop do
+      begin
+        sign = Game.input
+
+        if defined?(FIRST_PLAYER) == 'constant' && FIRST_PLAYER.sign == sign || sign.match?(/[[:digit:]]/)\
+          || sign.length > 1
+          raise InvalidInput, 'Invalid input. Use a different letter!'
+        end
+      rescue InvalidInput => e
+        puts e
+        retry
+      else
+        return Player.new(name, sign)
+      end
+    end
+  end
 end
 
-class PlayerTwo < Players
-
-end
+puts FIRST_PLAYER = Game.create_player
+puts SECOND_PLAYER = Game.create_player
 
 board = Board.new
 puts board
