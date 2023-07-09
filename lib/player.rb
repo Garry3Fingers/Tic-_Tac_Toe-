b8a39bbd7c08f7b82e826e145ee5a8fdd752f488
Garry3Fingers <www.vlad78@yandex.ru>
2023-07-09 15:01:26 +0300

    Move class Player to player.rb

---
+++ lib/player.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# Class shows state of player
+class Player
+  attr_reader :name, :sign
+
+  def initialize(name, sign)
+    @name = name
+    @sign = sign
+  end
+
+  def to_s
+    "Player: #{name}, sign: #{sign}."
+  end
+
+  def make_move
+    input = gets.chomp
+    unless input.match?(/[[:digit:]]/) && PlayGame::BOARD.move_board.any? { |arr| arr.include?(input.to_i) } == true
+      raise InvalidInput, 'Invalid input. Use a different number!'
+    end
+  rescue InvalidInput => e
+    puts e
+    retry
+  else
+    input.to_i
+  end
+end
+
+class InvalidInput < StandardError; end
