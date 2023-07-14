0c1eb11e70df3e71b6031ffc487b6a09e1894930
Garry3Fingers <www.vlad78@yandex.ru>
2023-07-14 21:14:05 +0300

    Refactor class

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -2,22 +2,22 @@
 
 # This class implements the game flow
 class PlayGame
-  attr_reader :first_player, :second_player, :board
+  attr_reader :first_player, :second_player, :game_board
 
   def initialize(args)
     @first_player = args[:first_player]
     @second_player = args[:second_player]
-    @board = args[:board]
+    @game_board = args[:board]
   end
 
   def play_game
-    puts board
+    puts game_board
     loop do
       first_player_move
-      break if winner_check
+      break if WinnerCheckWrapper.winner_check(args)
 
       second_player_move
-      break if winner_check
+      break if WinnerCheckWrapper.winner_check(args)
     end
   end
 
@@ -25,66 +25,21 @@ class PlayGame
 
   def first_player_move
     puts "#{first_player.name} make your move!"
-    board.process_player_move(first_player.sign, first_player.make_move(board))
+    game_board.process_player_move(first_player.sign, first_player.make_move(game_board))
   end
 
   def second_player_move
     puts "#{second_player.name} make your move!"
-    board.process_player_move(second_player.sign, second_player.make_move(board))
+    game_board.process_player_move(second_player.sign, second_player.make_move(game_board))
   end
 
-  def check_left_moves
-    return unless board.board.any? { |arr| arr.any?(Integer) } == false
-
-    puts 'It\'s a draw!'
-    true
-  end
-
-  def first_player_won
-    "Congratulations! #{first_player.name} won the game!"
-  end
-
-  def second_player_won
-    "Congratulations! #{second_player.name} won the game!"
-  end
-
-  def first_column
-    [].push(board.board_for_check[0][0], board.board_for_check[1][0], board.board_for_check[2][0])
-  end
-
-  def second_column
-    [].push(board.board_for_check[0][1], board.board_for_check[1][1], board.board_for_check[2][1])
-  end
-
-  def third_column
-    [].push(board.board_for_check[0][2], board.board_for_check[1][2], board.board_for_check[2][2])
-  end
-
-  def left_diagonal
-    [].push(board.board_for_check[0][0], board.board_for_check[1][1], board.board_for_check[2][2])
-  end
-
-  def right_diagonal
-    [].push(board.board_for_check[0][2], board.board_for_check[1][1], board.board_for_check[2][0])
-  end
-
-  def check_arr(first_column, second_column, third_column, left_diagonal, right_diagonal)
-    check_arr = board.board_for_check.push(first_column, second_column, third_column, left_diagonal, right_diagonal)
-    check_arr.any? do |arr|
-      if arr.all?(first_player.sign)
-        puts first_player_won
-        return true
-      elsif arr.all?(second_player.sign)
-        puts second_player_won
-        return true
-      end
-    end
-  end
-
-  def winner_check
-    return unless check_arr(first_column, second_column, third_column, left_diagonal, right_diagonal)\
-    || check_left_moves
-
-    true
+  def args
+    {
+      board: game_board,
+      first_name: first_player.name,
+      second_name: second_player.name,
+      first_sign: first_player.sign,
+      second_sign: second_player.sign
+    }
   end
 end
