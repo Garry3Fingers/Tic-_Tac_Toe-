35e24eef1d560e08107fe37f3c4a0620bfa33785
Garry3Fingers <www.vlad78@yandex.ru>
2023-07-16 18:09:25 +0300

    Add tests for class WinnerCheck

---
+++ spec/winner_check_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative '../lib/winner_check'
+
+describe WinnerCheck do
+  describe '#check' do
+    args = {
+      first_sign: 'V',
+      second_sign: 'N',
+      board: [%w[V V V], [2, 2, 2], [3, 3, 3]],
+      first_name: 'Vlad',
+      second_name: 'Natasha'
+    }
+    subject(:winner) { described_class.new(args) }
+    before do
+      allow(winner).to receive(:puts)
+    end
+
+    it 'returns true' do
+      expect(winner.check).to be(true)
+    end
+
+    it 'returns true when is there is no move left' do
+      winner.instance_variable_set(:@game_board, [%w[V V N], %w[N V V], %w[V N N]])
+      expect(winner.check).to be(true)
+    end
+
+    it "doesn't return true" do
+      winner.instance_variable_set(:@game_board, [[1, 1, 1], [2, 2, 2], [3, 3, 3]])
+      expect(winner.check).not_to be(true)
+    end
+  end
+end
